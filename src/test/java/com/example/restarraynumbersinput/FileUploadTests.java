51024b9437bd351c0e712265a0f5d9c09c0a257f
asm <vaskocuturilo@rambler.ru>
2023-03-13 18:15:25 +0100

    DV-00004: Add unit test for upload file.

---
+++ src/test/java/com/example/restarraynumbersinput/FileUploadTests.java
@@ -0,0 +1,45 @@
+package com.example.restarraynumbersinput;
+
+import com.example.restarraynumbersinput.service.StorageFileService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class FileUploadTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private StorageFileService storageFileService;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+
+    @Test
+    public void whenFileUpload_thenCheckStatusIsOk() throws Exception {
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+                "file",
+                "upload.txt",
+                MediaType.TEXT_PLAIN_VALUE,
+                "Test upload file".getBytes());
+
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        mockMvc.perform(multipart("/api/v1/numbers/upload").file(mockMultipartFile))
+                .andExpect(status().isOk());
+
+        then(this.storageFileService).should().saveFile(mockMultipartFile);
+    }
+}
